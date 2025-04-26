2d2c1a9efcd5ed5d9a08778cb34a07461ca5a933
rion-5 <sklee@hanyang.ac.kr>
2025-04-26 20:17:42 +0900

    edit test svg map

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,15 +1,16 @@
 <script>
   // 도담 열람실 맵 데이터 (엑셀 기반)
   const map = [
-    [null, null, null, null, null, null, null, null, null, 52],
-    [6, 7, 8, 9, 10, null, 41, 42, null, 53],
-    [11,12,13,14,15, null, 43,44, null,54],
-    [null,null,null,null,null,null,null,null,null,55],
-    [16,17,18,19,20, null,45,46, null,56],
-    [21,22,23,24,25, null,47,48, null,57],
-    [null,null,null,null,null,null,null,null,null,58],
-    [26,27,28,29,30, null,49,50, null,59],
-    [31,32,33,34,35, null,null,null,null,60],
+    [1, 2, 3, 4, 5, null, 39, 40, null, 51],
+    [6, 7, 8, 9, 10, null, 41, 42, null, 52],
+    [11,12,13,14,15, null, 43,44, null,53],
+    [null,null,null,null,null,null,null,null,null,54],
+    [16,17,18,19,20, null,45,46, null,55],
+    [21,22,23,24,25, null,47,48, null,56],
+    [null,null,null,null,null,null,null,null,null,57],
+    [26,27,28,29,30, null,49,50, null,58],
+    [31,32,33,34,35, null,null,null,null,59],
+    [null,null,36,37,38, null,null,null,null,60],
   ];
 
   const cellWidth = 40;
@@ -31,7 +32,7 @@
             y={rowIndex * (cellHeight + padding)}
             width={cellWidth}
             height={cellHeight}
-            fill="#9BE29B"
+            fill="green"
             stroke="black"
             stroke-width="1"
             rx="4"
