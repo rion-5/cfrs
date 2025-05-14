c3efd74e6f062b583eae4a2d3ab4246c813e954e
rion-5 <sklee@hanyang.ac.kr>
2025-05-14 17:25:41 +0900

    edit crontab query

--- scripts/auto-end-seats.sql
+++ scripts/auto-end-seats.sql
@@ -2,4 +2,4 @@
 UPDATE reading_seats
 SET end_time = CURRENT_TIMESTAMP, updated_at = CURRENT_TIMESTAMP
 WHERE end_time IS NULL
-  AND start_time <= CURRENT_TIMESTAMP - INTERVAL '20 minutes';
+  AND start_time <= CURRENT_TIMESTAMP - INTERVAL '3 hours';
